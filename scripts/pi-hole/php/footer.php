159ff880ce6d47d3ee3e489431897a428010ad5e
XhmikosR <xhmikosr@gmail.com>
2020-05-19 08:49:47 +0300

    Move scripts to the footer.
    Signed-off-by: XhmikosR <xhmikosr@gmail.com>

--- scripts/pi-hole/php/footer.php
+++ scripts/pi-hole/php/footer.php
@@ -66,6 +66,23 @@
     </footer>
 </div>
 <!-- ./wrapper -->
+
+<script src="scripts/vendor/jquery.min.js"></script>
+<script src="scripts/vendor/jquery-ui.min.js"></script>
+<script src="style/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="scripts/vendor/adminlte.min.js"></script>
+<script src="scripts/vendor/bootstrap-notify.min.js"></script>
+
+<?php if(in_array($scriptname, array("groups.php", "groups-clients.php", "groups-domains.php", "groups-adlists.php"))){ ?>
+<script src="scripts/vendor/bootstrap-select.min.js"></script>
+<script src="scripts/vendor/bootstrap-toggle.min.js"></script>
+<?php } ?>
+
+<script src="scripts/vendor/datatables.min.js"></script>
+<script src="scripts/vendor/moment.min.js"></script>
+<script src="scripts/vendor/Chart.min.js"></script>
+<script src="scripts/vendor/iCheck.min.js"></script>
+
 <script src="scripts/pi-hole/js/footer.js"></script>
 </body>
 </html>
